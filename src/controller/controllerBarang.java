4fc64338a0adc414d282cf3475991dd7be6d9aa4
zaidanaljauhar <zaidan_23520002@stimata.ac.id>
2024-05-15 12:52:21 +0700

    inventori

--- src/controller/controllerBarang.java
+++ src/controller/controllerBarang.java
@@ -4,10 +4,80 @@
  */
 package controller;
 
+import dao.daoBarang;
+import java.util.List;
+import javax.swing.JOptionPane;
+import model.*;
+import view.FormBarang;
 /**
  *
  * @author user
  */
 public class controllerBarang {
-    
+    FormBarang frame;
+    List<Barang> listBrg;
+    daoBarang daoBrg = new daoBarang();
+    Barang brg = new Barang();
+
+    public controllerBarang(FormBarang frame){
+        this.frame = frame;
+        listBrg = daoBrg.getData();
+    }
+
+    public void tampil_tabel(){
+        TabelModelBarang tabelBrg = new TabelModelBarang(listBrg);
+        frame.getTblBarang().setModel(tabelBrg);
+    }
+
+    public void tambahData(){
+        if(frame.getTxtKode().getText().equals("")){
+            JOptionPane.showMessageDialog(null, "Kode Barang belum diisi");
+
+        }else {
+            brg.setKode(frame.getTxtKode().getText());
+            brg.setNama(frame.getTxtNama().getText());
+            brg.setJumlah(Integer.parseInt(frame.getTxtJumlah().getText()));
+            brg.setHarga(Integer.parseInt(frame.getTxtJumlah().getText()));
+            brg.setMerek(frame.getTxtMerek().getText());
+            daoBrg.tambah(brg);
+            JOptionPane.showMessageDialog(frame, "Berhasil menambahkan data baru");
+        }
+    }
+
+
+     public void ubahData(){
+        if(frame.getTxtKode().getText().equals("")){
+            JOptionPane.showMessageDialog(null, "Kode Barang belum diisi");
+
+        }else {
+            brg.setKode(frame.getTxtKode().getText());
+            brg.setNama(frame.getTxtNama().getText());
+            brg.setJumlah(Integer.parseInt(frame.getTxtJumlah().getText()));
+            brg.setHarga(Integer.parseInt(frame.getTxtJumlah().getText()));
+            brg.setMerek(frame.getTxtMerek().getText());
+            daoBrg.ubah(brg);
+            JOptionPane.showMessageDialog(frame, "Berhasil menambahkan data baru");
+        }
+    }
+
+     public void hapusData(){
+         brg.setKode(frame.getTxtKode().getText());
+         daoBrg.hapus(brg);
+         JOptionPane.showMessageDialog(frame, "Berhasil Mengahpaus data");
+     }
+
+    public void bersih(){
+        frame.setTxtKode("");
+        frame.setTxtNama("");
+        frame.setTxtJumlah(0);
+        frame.setTxtHarga(0);
+        frame.setTxtMerek("");
+    }
+
+
+    public void keluar(){
+        frame.dispose();
+    }
+
+
 }
--- src/dao/daoBarang.java
+++ src/dao/daoBarang.java
@@ -4,7 +4,7 @@
  */
 package dao;
 
-import 
+import controller.koneksi;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -12,73 +12,114 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
-import java.swing.JOptionPane;
+import javax.swing.JOptionPane;
 import model.Barang;
-
 
 /**
  *
  * @author user
  */
 public class daoBarang {
-    connection connection;
-    final String insert="INSERT INTO barang (kode, nama, jumlah, harga, merek) VALUES (?,?,?,?,?);";
-    final String insert="UPDATE barang SET nama=?, jumlah=?, harga=?, merek=?, WHERE kode=?;";
-    final String delete="DELETE FROM Barang WHERE kode=?;";
-    final String select="SELECT * FROM Barang ORDER BY KODE ASC;";
-    final String selectData="SELECT * FROM barang where kode=?;";
-    //final String cekKode="SELECT * FROM barang where kode=?;";
-}
+    Connection connection;
+    final String insert = "INSERT INTO barang (kode, nama, jumlah, harga, merek) VALUES (?, ?, ?, ?, ?);";
+    final String update = "UPDATE barang SET nama=?, jumlah=?, harga=?, merek=? WHERE kode =?;";
+    final String delete = "DELETE FROM barang WHERE kode=?;";
+    final String select = "SELECT * FROM barang ORDER BY kode ASC;";
+    final String selectData = "SELECT * FROM barang WHERE kode=?;";
 
-    public daoBarang() {
+    public daoBarang(){
         connection = koneksi.connection();
-}
+    }
 
-    public void tambah(Barang brg) {
-        preparedStatement = null;
+    public void tambah(Barang brg){
+        PreparedStatement statement = null;
         try {
             statement = connection.prepareStatement(insert);
             statement.setString(1, brg.getKode());
-            statement.setString(2, brg.getNama()); 
+            statement.setString(2, brg.getNama());
             statement.setInt(3, brg.getJumlah());
             statement.setInt(4, brg.getHarga());
             statement.setString(5, brg.getMerek());
             statement.executeUpdate();
-    }catch (SQLException ex) {
-    }
-}
+        } catch(SQLException ex) {
 
-    public void ubah(Barang brg) {
-        PrepareStatement statement = null;
+        }
+    }
+
+    public void ubah(Barang brg){
+        PreparedStatement statement = null;
         try {
             statement = connection.prepareStatement(update);
-            statement.setString(1, brg.getNama());
-            statement.setInt(2, brg.getJumlah()); 
-            statement.setInt(3, brg.getHarga());
-            statement.setString(4, brg.getMerek());
-            statement.setString(5, brg.getKode());
+            statement.setString(1, brg.getKode());
+            statement.setString(2, brg.getNama());
+            statement.setInt(3, brg.getJumlah());
+            statement.setInt(4, brg.getHarga());
+            statement.setString(5, brg.getMerek());
             statement.executeUpdate();
-        } catch (SQLException ex) {
+        } catch(SQLException ex) {
+
         }
     }
 
-    public void hapus(Barang brg) {
-        PrepareStatement statement = null;
+       public void hapus(Barang brg){
+        PreparedStatement statement = null;
         try {
-          statement = connection.prepareStatement(delete);
-          statement.setString(1, brg.getKode());
-          statement.executeUpdate();
-          } catch (SQLException ex) {
-          }
-     }
+            statement = connection.prepareStatement(delete);
+            statement.setString(1, brg.getKode());
+            statement.executeUpdate();
+        } catch(SQLException ex) {
 
-    public void hapus(Barang brg) {
-        PrepareStatement statement = null;
-        try {
-          statement = connection.prepareStatement(selectData);
-          statement.setString(1, brg.getKode());
-          statement.executeUpdate();
-          } catch (SQLException ex) {
-             JOptionpane.showMessageDialog(null, ex);
         }
     }
+
+
+       public void tampil(Barang brg){
+        PreparedStatement statement = null;
+        try {
+            statement = connection.prepareStatement(selectData);
+            statement.setString(1, brg.getKode());
+            statement.executeUpdate();
+        } catch(SQLException ex) {
+            JOptionPane.showMessageDialog(null, ex);
+        }
+    }
+
+       public List<Barang> getData(){
+           List<Barang> listBrg = null;
+           try{
+               listBrg = new ArrayList<>();
+               Statement st = connection.createStatement();
+               ResultSet rs = st.executeQuery(select);
+               while(rs.next()){
+                   Barang brg = new Barang();
+                   brg.setKode(rs.getString("kode"));
+                   brg.setNama(rs.getString("nama"));
+                   brg.setJumlah(rs.getInt("jumlah"));
+                   brg.setHarga(rs.getInt("harga"));
+                   brg.setMerek(rs.getString("merek"));
+                   listBrg.add(brg);
+               }
+
+           }catch(SQLException ex) {
+            JOptionPane.showMessageDialog(null, ex);
+        }
+           return listBrg;
+       }
+
+       public int cekKode(String kode){
+           PreparedStatement statement = null;
+           int ketemu = 0;
+           try {
+               statement = connection.prepareStatement(selectData);
+               statement.setString(1, kode);
+               ResultSet rs = statement.executeQuery();
+               while(rs.next()){
+                   ketemu++;
+               }
+           }catch(SQLException ex){
+
+           }
+           return ketemu;
+       }
+
+}
